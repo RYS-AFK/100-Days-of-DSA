751e8ddcf347d01107ad676aca2bbe25c424c70e
RYS-AFK <RAOYASHVIR16@GMAIL.COM>
2026-04-25 23:07:51 +0530

    Add files via upload

---
+++ day84.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+           greater than key, to one position ahead
+           of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n;
+    
+    // Input the size of the array
+    if (scanf("%d", &n) != 1) return 0;
+    
+    int arr[n];
+    
+    // Input n space-separated integers
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Perform Insertion Sort
+    insertionSort(arr, n);
+
+    // Print the sorted array
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
